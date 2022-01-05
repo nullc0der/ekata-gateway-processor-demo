fd3fba7766f6eafd72d827f02145d39486030975
Prasanta Kakati <prasantakakati1994@gmail.com>
2022-01-06 03:40:06 +0530

    optimized css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: {
+        enabled: true,
+        content: ["./templates/**/*.html"],
+    },
+    darkMode: "media", // or 'media' or 'class',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
